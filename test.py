5e20d8b8c2f3104e4e4b0308a53062ec6461eaf6
ouyang_bro <ouyangjianguo@zizaihome.com>
2018-02-02 17:10:18 +0800

    test：用学生管理来做test case更加平易近人...

--- test.py
+++ test.py
@@ -9,35 +9,26 @@ from BaseDao import BaseDao
 from BaseInfo import BaseInfo
 
 
-class GongxiuInfo(BaseInfo):
+class StudentInfo(BaseInfo):
     def __init__(self, _item):
         self.id = 0
         self.name = ''
-        self.creator_id = 0
-        self.start_time = ''
-        self.end_time = ''
-        self.type = 0
-        self.detail = ''
-        self.img_id = 0  # 图片id
-        self.join_cnt = 0
-        self.status = 0  # 审核 0 - 未审核， 1 - 审核通过
         self.del_flag = 0
         self.create_time = ''
         self.update_time = ''
 
         if isinstance(_item, dict):
-            for key, value in _item.items():
+            for key, value in _item.iteritems():
                 self.__dict__[key] = value
 
 
-class GongxiuDao(BaseDao):
-    DataInfo = GongxiuInfo
-    table_name = 'gongxiu'  # 数据库表名
-    escape_list = ['name', 'detail']  # 需要转义的list
-    quot_list = ['start_time', 'end_time', 'img_key', 'create_time', 'update_time']  # 需要带引号的list
-    not_append_list = ['creator_id', 'type', 'status', 'img_id',
-                       'del_flag']  # int list，但是不可能有append操作的list，如 img_id
-    append_list = ['join_cnt']  # int list, 但是可能有append操作的list，如add_cnt, view_cnt
+class StudentDao(BaseDao):
+    DataInfo = StudentInfo
+    table_name = 'student'  # 数据库表名
+    escape_list = ['name']  # 需要转义的list
+    quot_list = ['create_time', 'update_time']  # 需要带引号的list
+    not_append_list = ['del_flag']  # int list，但是不可能有append操作的list，如 img_id
+    append_list = []  # int list, 但是可能有append操作的list，如add_cnt, view_cnt
 
 
 @tornado.gen.coroutine
@@ -50,31 +41,31 @@ def main():
             # the context for app request...
             context = None
 
-            gongxiu_id = 1
-            user_id = 1001
+            stu_id = 1
+            stu_name = "tom"
 
-            # get the gongxiu which id=1, user_id=1001
-            user = yield GongxiuDao.get_by_cols(context, conn, where_lst=[
+            # get the student which id=1, user_id=1001
+            user = yield StudentDao.get_by_cols(context, conn, where_lst=[
                 {
-                    'key': gongxiu_id,
-                    'where_col': 'gongxiu_id',
+                    'key': stu_id,
+                    'where_col': 'id',
                     'col_str': False
                 },
                 {
-                    'key': user_id,
-                    'where_col': 'user_id',
-                    'col_str': False
+                    'key': stu_name,
+                    'where_col': 'name',
+                    'col_str': True
                 }
             ], is_fetchone=True, with_del=True)
             logging.info("get_by_cols:")
             logging.info(user)
 
-            # update join_cnt += 1
+            # update age += 1
             dic = {
-                'id': gongxiu_id,
-                'join_cnt': (1, True)
+                'name': stu_name,
+                'age': (1, True)
             }
-            yield GongxiuDao.update(context, conn, dic)
+            yield StudentDao.update(context, conn, dic)
             yield conn.commit()
     except Exception, ex:
         logging.error(ex, exc_info=1)
